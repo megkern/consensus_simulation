b7bb4e1b274de65660f9c2ca8325b91c3c11ddef
megkern <99364827+megkern@users.noreply.github.com>
2024-10-09 11:03:27 -0500

    Add files via upload

---
+++ consensus_voting.m
@@ -0,0 +1,171 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PARAMETERS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear global;
+
+global SimParams = struct(
+    "voting",
+    struct(
+        % numChoices is the number of individual items that are available to vote upon
+        "numChoices", 60,
+
+        % participants is the number of participants being simulated
+        "participants", [40:1:50],
+
+        % votesPP is how many votes each participant can cast
+        "votesPP", [3:1:15],
+
+        % method describes how the participants distribute their votes
+        % available methods:
+        %
+        % 'linear':
+        %       All participants agree that 1 is best and that 'numChoices' is worst.
+        %
+        % 'random':
+        %       Participants randomly allocate votes. Only 1 vote allowed per item - no vote stacking
+        %
+        % 'random-groupings-single-faction':
+        %       The items to be voted upon are grouped (according to 'choice_groups.boundaries', below).
+        %       All agree that the first group is best, next is 2nd best, etc. But within each group
+        %       participants vote randomly.
+        %
+        % 'random-groupings-multi-faction':
+        %       Like 'random-groupings-single-faction', but users are divided into factions (according
+        %       to 'factions.boundaries', below), which in general do not agree on the ordering of
+        %       group preferences from favorite to least favorite.
+        %
+        % 'random-MV'
+        %       Like 'random', but allows users to stack votes.
+        "method", 'random-groupings-multi-faction'
+    ),
+    "csr_criteria",
+    struct(
+        % Fractions for determining consensus, saturation, and rejection values
+        "consensus_fraction", 0.8,
+        "saturation_fraction", 1.0,
+        "rejection_fraction", 0.1
+    ),
+    "choice_groups",
+    struct(
+        % boundaries specifies the distribution of how items being voted on are divided into
+        % different preference groups
+        % e.g., [0.25 0.5 0.75 1.0] defines the dividing lines to be every 25% of the total number
+        % of items, so if there were 100 items, group 1 would contain 1-25, group 2 would have 26-50, etc.
+        % the number of boundaries and their values can be changed, so for example if you wanted 3 groups
+        % where the first one is 10% of the total, the next one is 75% and the last one is 15%, you could
+        % use [0.1 0.85 1.0]
+        "boundaries", [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
+    ),
+    "factions",
+    struct(
+        % boundaries specifies the distribution of people into the various factions. The dividing lines
+        % operate similarly to group boundaries, above. As with group boundaries, factions do not need to be
+        % the same size as one another, and they can be added and taken away by the manner in which 'boundaries'
+        % is edited.
+        "boundaries", [0.25 0.5 0.75 1.0],
+
+        % preferences determines the order of preference that each faction has for each choice group.
+        % the rows represent factions 1-N, and the columns represent the order of preference for that faction,
+        % with the 1st column being the favorite, and the last column the least favogrite.
+        %
+        % NOTE that it is important that the preferences array match the dimensions implied by the number of choice
+        % groups (columns), and the number of factions (rows).
+        "preferences", [1 2 3 4 5 6 7 8 9 10;
+                        4 1 2 3 6 5 8 7 9 10;
+                        3 4 1 2 5 6 7 8 9 10;
+                        2 3 4 1 6 5 8 7 9 10],
+
+        % vote_distribution represents what fraction of total available votes a participant will cast in each
+        % of their descending list of preferences. For example, [ 0.5 0.35 0.15 ] would mean that a voter
+        % would allocate 50% of their votes to their favorite choice group, 35% of their votes to their 2nd favorite
+        % group, and 15% of their votes to their 3rd favorite group.
+        %
+        % The sum of the columns should be 1.0
+        "vote_distribution", [ 0.3 0.25 0.2 0.15 0.1 ]
+    ),
+    "precision",
+    struct(
+        % epsilon and deltaExitCriteria define the conditions under which
+        % the simulation ends. When the 'consensus' average value has
+        % changed less than 'epsilon' for at least 'deltaExitCriteria' passes,
+        % the simulation is complete
+        "epsilon", 0.001,
+        "deltaExitCriteria", 10
+    ),
+    "debug",
+    struct(
+        % set display_vote_monitor to true to show a bar graph of voting results during the simulation,
+        % set display_vote_monitor to false to turn it off
+        "display_vote_monitor", true
+    )
+);
+
+%
+% Allocate Memory
+%
+c = s = r = n = zeros(length(SimParams.voting.participants), length(SimParams.voting.votesPP));
+
+%
+% Set up bar monitor plot window
+%
+if SimParams.debug.display_vote_monitor == true
+    ss = get(0, 'screensize');
+    width = ceil(0.4 * ss(3));
+    height = ceil(0.67 * width);
+    x = ceil(0.55 * ss(3));
+    y = ceil(0.25 * ss(4));
+    figure('Position', [x y width height]);
+endif
+
+%
+% Run the simulations for each # of participants and # of votes per participant
+%
+for i = 1:length(SimParams.voting.participants)
+  for j = 1:length(SimParams.voting.votesPP)
+    fprintf('Simulating %2d participants %2d votes: ', SimParams.voting.participants(i), SimParams.voting.votesPP(j));
+    [c(i,j), s(i,j), r(i,j), n(i,j)] = simulate_voting(SimParams.voting.participants(i), SimParams.voting.votesPP(j));
+    fprintf('\b  [c = %f, s = %e, r = %f, n = %d]\n', c(i,j), s(i,j), r(i,j), n(i,j));
+  end
+end
+
+%
+% Plot Results
+%
+width = ceil(0.8 * ss(3));
+height = ceil(0.25 * width);
+x = 0.5 * (ss(3) - width);
+y = 0.5 * (ss(4) - height);
+figure("Position", [x y width height]);
+subplot(1,4,1);
+axis off;
+str = disp(SimParams);
+underscores = strfind(str,'_');
+str(underscores) = ' ';
+text(0,0.5,str);
+
+subplot(1,4,2);
+[x,y] = meshgrid(SimParams.voting.participants, SimParams.voting.votesPP);
+mesh(x, y, c');
+title(strcat("Consensus"," ( c >= ", sprintf("%4.2f ",SimParams.csr_criteria.consensus_fraction),")"), "FontSize", 16);
+xlabel("# of Participants", "FontSize", 14);
+ylabel("# of Votes per Participant", "FontSize", 14);
+zlabel("Fraction", "FontSize", 14);
+
+subplot(1,4,3);
+[x,y] = meshgrid(SimParams.voting.participants, SimParams.voting.votesPP);
+mesh(x, y, r');
+title(strcat("Rejected"," ( r <= ", sprintf("%4.2f ",SimParams.csr_criteria.rejection_fraction),")"), "FontSize", 16);
+xlabel("# of Participants", "FontSize", 14);
+ylabel("# of Votes per Participant", "FontSize", 14);
+zlabel("Fraction", "FontSize", 14);
+
+subplot(1,4,4);
+[x,y] = meshgrid(SimParams.voting.participants, SimParams.voting.votesPP);
+mesh(x, y, s');
+title(strcat("Saturated"," ( s >= ", sprintf("%4.2f ",SimParams.csr_criteria.saturation_fraction),")"), "FontSize", 16);
+xlabel("# of Participants", "FontSize", 14);
+ylabel("# of Votes per Participant", "FontSize", 14);
+zlabel("Fraction", "FontSize", 14);
+
+
---
+++ simulate_voting.m
@@ -0,0 +1,222 @@
+%
+% Voting simulation function
+%
+% Parameters:
+%         SimParams: See above.
+%               p_n: number of participants in this run
+%               p_v: number of votes per participant this run
+%
+% Returns:
+%          cons_avg: Average computed consensus (0,1)
+%          dilu_avg: Average computed dilution (0,1)
+%                 n: Number of iterations required to achieve exit condition
+%
+function [cons_avg, satu_avg, reje_avg, n] = simulate_voting(p_n, p_v)
+    global SimParams;
+    n = 0;
+    cons_avg = 0;
+    satu_avg = 0;
+    reje_avg = 0;
+    deltaCounts = 0;
+    prefs = zeros(p_n, SimParams.voting.numChoices);
+    choiceVotes = zeros(1,SimParams.voting.numChoices);
+    sw='\\\\\\\\\\\|||||||||||///////////-----------';
+
+    % Rejection Labeling
+    while deltaCounts < SimParams.precision.deltaExitCriteria
+        % generate participant preferences
+        for i = 1:p_n
+            prefs(i,:) = generate_prefs(i, p_n, p_v);
+        end
+
+        % Add up their votes
+        choiceVotes = zeros(1,SimParams.voting.numChoices);
+        for i = 1:p_n
+            for j = 1:p_v
+              if prefs(i,j) != 0
+                    choiceVotes(prefs(i,j)) = choiceVotes(prefs(i,j)) + 1;
+              end
+            end
+        end
+
+        % Compute consensus, dilution, rejected
+        cons = consensus(choiceVotes, p_n);
+        satu = saturation(choiceVotes, p_n);
+        reje = rejected(choiceVotes, p_n);
+
+        % Calculate weighted averages
+        cons_avg_new = (cons_avg * n + cons) / (n + 1);
+        satu_avg_new = (satu_avg * n + satu) / (n + 1);
+        reje_avg_new = (reje_avg * n + reje) / (n + 1);
+
+        delta_cons = abs(cons_avg - cons_avg_new);
+        delta_satu = abs(satu_avg - satu_avg_new);
+
+        delta = delta_cons; % was this: max( delta_cons, delta_satu );
+
+        if delta < SimParams.precision.epsilon
+            deltaCounts = deltaCounts + 1;
+
+            if SimParams.debug.display_vote_monitor == true
+                bar( choiceVotes );
+                textVertOffset = 0.5;
+                axis([0 SimParams.voting.numChoices 0 max(SimParams.voting.participants)]);
+                line([0 SimParams.voting.numChoices], [1 1]*SimParams.csr_criteria.rejection_fraction*p_n, 'color', 'red');
+                text(0.95*SimParams.voting.numChoices, SimParams.csr_criteria.rejection_fraction*p_n+textVertOffset, 'Rejection', ...
+                     'fontsize', 12, 'color', 'black', 'horizontalAlignment', 'right');
+
+                % Consensus Labeling
+                line([0 SimParams.voting.numChoices], [1 1]*SimParams.csr_criteria.consensus_fraction*p_n, 'color', 'green');
+                text(0.95*SimParams.voting.numChoices, SimParams.csr_criteria.consensus_fraction*p_n+textVertOffset, 'Consensus', ...
+                     'fontsize', 12, 'color', 'black', 'horizontalAlignment', 'right');
+
+                % Saturation Labeling
+                line([0 SimParams.voting.numChoices], [1 1]*SimParams.csr_criteria.saturation_fraction*p_n, 'color', 'blue');
+                text(0.95*SimParams.voting.numChoices, SimParams.csr_criteria.saturation_fraction*p_n+textVertOffset, 'Saturation',
+                     'fontsize', 12, 'color', 'black', 'horizontalAlignment', 'right');
+
+                xlabel('Choices');
+                ylabel('Total Votes');
+                title(sprintf('Voting Bar Graph, %d Participants, %d Votes Each', p_n, p_v));
+                drawnow;
+            endif
+        else
+            deltaCounts = 0;
+        end
+
+        cons_avg = cons_avg_new;
+        satu_avg = satu_avg_new;
+        reje_avg = reje_avg_new;
+
+        % increment the loop counter
+        n = n + 1;
+
+        % print progress widget
+        % fprintf('\b%c', sw(mod(n,length(sw))+1));
+    end
+end
+
+%
+% Consensus
+% What fraction of choices received more than 'frac' percentage of people voting for it?
+%
+function [c] = consensus(choiceVotes, p_n)
+    global SimParams;
+    c = sum(choiceVotes >= SimParams.csr_criteria.consensus_fraction * p_n) ...
+            / SimParams.voting.numChoices;
+end
+
+%
+% Saturation
+% What fraction of choices received the maximum number of votes?
+%
+function [d] = saturation(choiceVotes, p_n)
+    global SimParams;
+    d = sum(choiceVotes >= SimParams.csr_criteria.saturation_fraction * p_n) ...
+            / SimParams.voting.numChoices;
+end
+
+%
+% Rejected
+% What fractionm of choices received less than 'frac' percentage of people voting for it?
+%
+function [s] = rejected(choiceVotes, p_n)
+    global SimParams;
+    s = sum(choiceVotes <= SimParams.csr_criteria.rejection_fraction * p_n) ...
+            / SimParams.voting.numChoices;
+end
+
+%
+% Generate Participant Prefs
+%
+% p_idx = participant index
+function [p] = generate_prefs(p_idx, p_n, p_v)
+    global SimParams;
+
+    switch SimParams.voting.method
+        case 'linear'
+            p = 1:SimParams.voting.numChoices;
+        case 'random'
+            p = randperm(SimParams.voting.numChoices);
+        case 'random-groupings-single-faction'
+            p = randomGSF(SimParams.voting.numChoices);
+        case 'random-groupings-multi-faction'
+            p = randomGMF(p_idx, p_n, p_v);
+        case 'random-MV'
+            p = randomMV(SimParams.voting.numChoices);
+        otherwise
+            error('Unsupported option for generate_prefs().');
+    endswitch
+end
+
+%
+% Randomize preferences in some number of groups, all participants assumed
+% to be in the same 'faction'.
+%
+function [p] = randomGSF(n)
+    global SimParams;
+
+    boundary_values = [0 SimParams.choice_groups.boundaries];
+
+    % Determine group boundary indices
+    b = round(n * [boundary_values]);
+    for i = 2:length(boundary_values)
+        p(b(i-1)+1:b(i)) = randperm(b(i)-b(i-1)) + b(i-1);
+    endfor
+end
+
+%
+% Randomize preferences in some number of groups, participants divided into
+% some number of non-overlapping factions
+%
+function [p] = randomGMF(p_idx, p_n, p_v)
+    global SimParams;
+
+    option_boundaries = [ 0 SimParams.choice_groups.boundaries];
+    faction_boundaries = SimParams.factions.boundaries;
+    fp = SimParams.factions.preferences;
+
+    b = round(SimParams.voting.numChoices * option_boundaries);
+    f = round(p_n * faction_boundaries);
+
+    % Determine the faction for this voter
+    faction = sum(f < p_idx) + 1;
+
+    numGroups = length(SimParams.choice_groups.boundaries);
+    numGroupsToVote = length(SimParams.factions.vote_distribution);
+    votesPerGroup = [round(SimParams.factions.vote_distribution * p_v) zeros(1,numGroups-numGroupsToVote)];
+
+    while sum(votesPerGroup) < p_v
+        % If not enough votes allocated, add to favorite group
+        votesPerGroup(1) = votesPerGroup(1) + 1;
+    endwhile
+    while sum(votesPerGroup) > p_v
+        % If too many votes allocated, take away from least favorite group that has
+        % at least one vote
+        for i = numGroupsToVote:-1:1
+            if votesPerGroup(i) > 0
+                votesPerGroup(i) = votesPerGroup(i) - 1;
+                break
+            endif
+        endfor
+    endwhile
+
+    % Determine preference groupings for that faction
+    p = zeros(1,SimParams.voting.numChoices);
+    votesTaken = 0;
+    for i = 2:length(option_boundaries)
+        p(votesTaken+1:votesTaken+votesPerGroup(i-1)) = randperm(b(i)-b(i-1),votesPerGroup(i-1)) + b(fp(faction,i-1));
+        votesTaken = votesTaken + votesPerGroup(i-1);
+    endfor
+
+end
+
+%
+% Randomize preferences but allow multiple votes on a choice for each user
+%   n = number of items to vote for
+function [p] = randomMV(n)
+    p = ceil(rand(1,n)*n);
+end
+
+
+
